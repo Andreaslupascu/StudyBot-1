9731708cf8c2bb413cd118ad331c57d01cb26f59
Andreas, lupascu <42746552+Andreaslupascu@users.noreply.github.com>
2022-02-16 03:03:16 +0000

    rewrote to use bot commands instead of regular commands

--- main.py
+++ main.py
@@ -5,24 +5,26 @@ from discord.ext import commands
 from StudyBot.discord_cog import DiscordCog
 
 load_dotenv()
-
-client = discord.Client()
-
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-
-client.run(os.environ['BOT_TOKEN'])
-
-
 bot = commands.Bot(command_prefix='!', help_command=None)
-bot.add_cog(DiscordCog(bot))
+
+#client = discord.Client()
+
+#@client.event
+@bot.event
+async def on_ready():
+    print('We have logged in as {}'.format(bot.user))
+
+#@client.event
+@bot.command(name="start", help= "starts a study timer")
+async def start_timer(ctx):
+  await ctx.send("time to work!")
+ #   if message.author == client.user:
+  #      return
+ #   if message.content.startswith('$hello'):
+ #      await message.channel.send('Hello!')
+
+#client.run(os.environ['BOT_TOKEN'])
+
+
+#bot.add_cog(DiscordCog(bot))
 bot.run(os.environ['BOT_TOKEN'])
